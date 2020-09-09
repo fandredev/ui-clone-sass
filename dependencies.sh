f3f766cd2bdc89df4754a0cada744296cf93d102
Felipe <profissionalf.andre@gmail.com>
2020-09-09 17:24:32 -0300

    fix: husky and lint-staged added

--- dependencies.sh
+++ dependencies.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-sudo yarn add styled-components react-router-dom && sudo yarn add eslint @types/styled-components @types/react-router-dom react-icons add prettier eslint-config-prettier eslint-plugin-prettier -D 
+sudo yarn add styled-components react-router-dom && sudo yarn add eslint @types/styled-components @types/react-router-dom react-icons add prettier eslint-config-prettier eslint-plugin-prettier husky lint-staged -D
